cb93f95ef147d474566b1eeea6dd215185d7bcc8
gaa_local <gaakun69@gmail.com>
2023-08-01 22:22:06 +0900

    ルーティング方法２
    django_appのurls.pyでインクルードを書きhelloのurls.pyでルーティングを行う

--- hello/urls.py
+++ hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #自分自身（helloプロジェクト）のindex関数を読む
+    #②「''」自分自身（helloプロジェクト）のindex関数を読む
+    # nameはviews.indexに名前をつけている
     path('', views.index, name='index'),
 ]
